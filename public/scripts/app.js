5728e02063971a6505e8959b475ad0d1db8f9748
danale <danale@danales-MacBook-Pro.local>
2018-02-15 09:58:13 -0500

    first commit

---
+++ public/scripts/app.js
@@ -0,0 +1,90 @@
+'use strict';
+
+console.log('App.js is running');
+
+var app = {
+  title: 'Decision',
+  subtitle: 'For binary life decisions, put your trust in a computer',
+  options: []
+};
+
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+
+  var option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    render();
+  }
+};
+
+var onRemoveAll = function onRemoveAll() {
+  app.options = [];
+  render();
+};
+// create "Remove All" button above list
+// on click -> wipe the array -> rerender
+
+var appRoot = document.getElementById('app');
+
+var render = function render() {
+  var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      app.title
+    ),
+    app.subtitle && React.createElement(
+      'p',
+      null,
+      app.subtitle
+    ),
+    React.createElement(
+      'p',
+      null,
+      app.options.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    React.createElement(
+      'p',
+      null,
+      app.options.length
+    ),
+    React.createElement(
+      'button',
+      { onClick: onRemoveAll },
+      'Remove All'
+    ),
+    React.createElement(
+      'ol',
+      null,
+      React.createElement(
+        'li',
+        null,
+        'Item one'
+      ),
+      React.createElement(
+        'li',
+        null,
+        'Item two'
+      )
+    ),
+    React.createElement(
+      'form',
+      { onSubmit: onFormSubmit },
+      React.createElement('input', { type: 'text', name: 'option' }),
+      React.createElement(
+        'button',
+        null,
+        'Add Option'
+      )
+    )
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+
+render();
---
+++ src/app.js
@@ -0,0 +1,52 @@
+console.log('App.js is running');
+
+const app = {
+  title: 'Decision',
+  subtitle: 'For binary life decisions, put your trust in a computer',
+  options: []
+};
+
+const onFormSubmit = e => {
+  e.preventDefault();
+
+  const option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    render();
+  }
+};
+
+const onRemoveAll = () => {
+  app.options = [];
+  render();
+};
+// create "Remove All" button above list
+// on click -> wipe the array -> rerender
+
+const appRoot = document.getElementById('app');
+
+const render = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <p>{app.options.length}</p>
+      <button onClick={onRemoveAll}>Remove All</button>
+      <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+
+render();
