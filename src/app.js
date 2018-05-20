f0714825556d829e30acf034809ed50001388a03
danale <danale@danales-MacBook-Pro.local>
2018-05-20 17:41:39 -0400

    Implemented Architecture and Header Styles

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DecisionTreeApp from './components/DecisionTreeApp';
+import './styles/styles.scss';
 
 ReactDOM.render(<DecisionTreeApp />, document.getElementById('app'));
