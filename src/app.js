8599477afebe9b6148c65085039dee6555cd2b2f
danale <danale@danales-MacBook-Pro.local>
2018-05-20 21:18:46 -0400

    Reset CSS

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import DecisionTreeApp from './components/DecisionTreeApp';
+import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 ReactDOM.render(<DecisionTreeApp />, document.getElementById('app'));
