e8eb93433c3c4b09299ec87299e72fde28a3ca2c
danale <danale@danales-MacBook-Pro.local>
2018-05-26 18:53:56 -0400

    first commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import DecisionTreeApp from './components/DecisionTreeApp';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<DecisionTreeApp />, document.getElementById('app'));
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
