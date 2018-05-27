1c8855da9878d046af0fa07f87033097dbfda8d5
danale <danale@danales-MacBook-Pro.local>
2018-05-27 19:20:37 -0400

    Set up React-Router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const DepenseDashboardPage = () => (
+  <div>This is from my dashbaord component</div>
+);
+
+const AddDepensePage = () => (
+  <div>This is my add expense component</div>
+);
+
+const EditDepensePage = () => (
+  <div>This is my edit expense component</div>
+);
+
+const HelpPage = () => (
+  <div>This is from my help component</div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route exact={true} path="/" component={DepenseDashboardPage} />
+      <Route path="/create" component={AddDepensePage} />
+      <Route path="/edit" component={EditDepensePage} />
+      <Route path="/help" component={HelpPage} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
